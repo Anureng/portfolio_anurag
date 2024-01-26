394bab6b0535ee9880f0a38618368bf338ec8816
Anurag Sidhu <anuragsidhu72@gmail.com>
2024-01-26 18:48:29 +0530

    DONE

--- src/components/About1.jsx
+++ src/components/About1.jsx
@@ -1,35 +1,38 @@
 import React from 'react'
 import Image from 'next/image'
 function About1() {
-  return (
-   <div className='bg-white mt-12 lg:mt-0 flex flex-col justify-center items-center h-full p-20 sm:p-0 lg:h-3/5 '>
-    <div className='mb-3 text-xl border-b border-black font-bold'>
-    About
-    </div>
-    <div className='  flex lg:flex-row flex-col justify-center items-center lg:space-x-20 space-y-4'>
-        <div>
-            <Image height={350} width={350} src="/about.jpg" className="  shadow-2xl w-96" alt="" />
-        </div>
-            <div className='  bg-gray-200 rounded-xl space-y-4 p-4 w-screen lg:w-2/5 flex flex-col justify-center items-center'>
-        <div className=' w-fit flex space-x-4'>
-<div className='rounded-xl bg-white px-1 py-2 shadow-2xl w-fit'> 
-    10 Months <br /> Of Experience
-</div>
-<div className='rounded-xl bg-white px-1 w-fit py-2 shadow-2xl'>
-    10+<br/> Projects
-</div>
+    return (
+        <div className='bg-white mt-12 lg:mt-0 flex flex-col justify-center items-center h-full p-20 sm:p-0 lg:h-3/5 '>
+            <div className='mb-3 text-xl border-b border-black font-bold'>
+                About
             </div>
-            <div>
-            I have expertise in both <em className='font-bold text-lg'> front-end and back-end development</em>, including popular frameworks like <em className='text-lg font-bold'> React.js, and Next.js </em>. My skills in back-end development include database management, server-side scripting, and API integration with <em className=' text-lg font-bold'> mongodb and Node.js</em>.  I also have experience in designing and developing blockchain-based applications using platforms such as <em className='text-lg font-bold'> Ethereum and polygon</em>.I can build <em className='text-lg font-bold'> DEX , DAO and DEFI based projects </em>. Feel free to explore my portfolio and get in touch with me if you have any questions or potential projects.
-            </div>
-            <a href="mailto:anuragsidhu72@gmail.com" target='_blank'>
-            <div className='bg-white px-1 py-2 rounded-xl'>Contact Me</div>
-            </a>
+            <div className='  flex lg:flex-row flex-col justify-center items-center lg:space-x-20 space-y-4'>
+                <div>
+                    <Image height={350} width={350} src="/about.jpg" className="  shadow-2xl w-96" alt="" />
+                </div>
+                <div className='  bg-gray-200 rounded-xl space-y-4 p-4 w-screen lg:w-2/5 flex flex-col justify-center items-center'>
+                    <div className=' w-fit flex space-x-4'>
+                        <div className='rounded-xl bg-white px-1 py-2 shadow-2xl w-fit'>
+                            3 Months <br /> Of Experience
+                        </div>
+                        <div className='rounded-xl bg-white px-1 w-fit py-2 shadow-2xl'>
+                            10+<br /> Projects
+                        </div>
+                    </div>
+                    {/* <div>
+                        I have expertise in both <em className='font-bold text-lg'> front-end and back-end development</em>, including popular frameworks like <em className='text-lg font-bold'> React.js, and Next.js </em>. My skills in back-end development include database management, server-side scripting, and API integration with <em className=' text-lg font-bold'> mongodb and Node.js</em>.  I also have experience in designing and developing blockchain-based applications using platforms such as <em className='text-lg font-bold'> Ethereum and polygon</em>.I can build <em className='text-lg font-bold'> DEX , DAO and DEFI based projects </em>. Feel free to explore my portfolio and get in touch with me if you have any questions or potential projects.
+                    </div> */}
 
+                    <div>I bring with me three months of hands-on experience in <em className='font-bold text-lg'> full-stack development at unicorniz innovation</em>, where I've honed my skills in crafting end-to-end solutions. My journey in the tech industry has been marked by a dedication to delivering seamless user experiences and pushing the boundaries of web development.</div>
+                    <div>In addition to my full stack development prowess, I've delved into the exciting world of blockchain development. My knowledge in this domain includes a solid understanding of smart contracts, decentralized applications (DApps), and the underlying technologies that power the blockchain ecosystem.</div>
+                    <a href="mailto:anuragsidhu72@gmail.com" target='_blank'>
+                        <div className='bg-white px-1 py-2 rounded-xl'>Contact Me</div>
+                    </a>
+
+                </div>
+            </div>
         </div>
-    </div>
-   </div>
-  )
+    )
 }
 
 export default About1
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,36 +6,39 @@ function Header() {
     <div className='h-screen bg-white  flex  items-center flex-col space-y-5 lg:space-y-0 mt-10 lg:mt-0'>
       <div className=' flex flex-col lg:flex-row p-10 lg:p-36  justify-center items-center space-y-8 lg:space-x-20  '>
         <div className='bg-gray-200 rounded-xl flex justify-center items-baseline  flex-col space-y-6 p-4 lg:w-3/6'>
-      <div className='font-bold text-xl'>
-        My self Anurag Sidhu
-      </div>
-      <div>While I may not have a lot of experience yet, I have been working hard to develop my skills.  I have completed <em className='font-bold text-lg'> Full Stack Web Development </em> and <em className='font-bold text-lg'> BlockChain Development </em>.  I am also continuously learning and exploring new techniques to improve my abilities.</div>
-      <div className='space-x-4'>
-        <a href="mailto:anuragsidhu72@gmail.com" target='_blank'>
-        <button className='bg-white rounded-xl px-1 py-2'>
-          Hired Me
-        </button>
-        </a>
-        <button className='bg-white rounded-xl px-1 py-2'>About</button>
-      </div>
-</div>
+          <div className='font-bold text-xl'>
+            My self Anurag Sidhu
+          </div>
+          {/* <div>
+            While I may not have a lot of experience yet, I have been working hard to develop my skills.  I have completed <em className='font-bold text-lg'> Full Stack Web Development </em> and <em className='font-bold text-lg'> BlockChain Development </em>.  I am also continuously learning and exploring new techniques to improve my abilities.</div> */}
+
+          <div>Greetings! I'm Anurag Sidhu, a passionate <em className='font-bold text-lg'> Full Stack Web Development </em>with a flair for creating captivating user interfaces. With a burgeoning interest in the world of technology, I've extended my expertise into the realm of <em className='font-bold text-lg'> Blockchain Development </em>, combining the art of frontend design with the innovation of decentralized systems.</div>
+          <div className='space-x-4'>
+            <a href="mailto:anuragsidhu72@gmail.com" target='_blank'>
+              <button className='bg-white rounded-xl px-1 py-2'>
+                Hired Me
+              </button>
+            </a>
+            <button className='bg-white rounded-xl px-1 py-2'>About</button>
+          </div>
+        </div>
         <div>
-        <Image
-          src="/user.JPG"
-          width={350}
-          height={350}
-           className="w-52 rounded-full shadow-2xl"
-           alt="Avatar" />
-  </div>
-  </div>
-  <div className=' space-x-5 '>
-    <SocialIcon  url='https://twitter.com/anurag_sidhu8'/>
-    <SocialIcon url='https://www.linkedin.com/in/anurag-sidhu-4b518521b/'/>
-    <SocialIcon url='https://github.com/Anureng'/>
-    <SocialIcon url='mailto:anuragsidhu72@gmail.com'/>
+          <Image
+            src="/user.JPG"
+            width={350}
+            height={350}
+            className="w-52 rounded-full shadow-2xl"
+            alt="Avatar" />
+        </div>
+      </div>
+      <div className=' space-x-5 '>
+        <SocialIcon url='https://twitter.com/anurag_sidhu8' />
+        <SocialIcon url='https://www.linkedin.com/in/anurag-sidhu-4b518521b/' />
+        <SocialIcon url='https://github.com/Anureng' />
+        <SocialIcon url='mailto:anuragsidhu72@gmail.com' />
 
 
-  </div>
+      </div>
     </div>
   )
 }
